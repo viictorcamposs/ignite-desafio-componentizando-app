3990fa82f9e12ceba58e344b4b4ebfa97db9fcbf
viictorcamposs <campos.viictor@gmail.com>
2021-03-26 11:57:13 -0300

    feat: add componente SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+import { GenreResponseProps } from './Content';
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number;
+  setSelectedGenreId: Function;
+}
+
+export function SideBar(props: SideBarProps) {
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
